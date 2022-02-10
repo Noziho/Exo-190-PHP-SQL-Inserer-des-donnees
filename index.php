f8245aaa21dd825d9bb745168290dc4a5eb7e1f6
Noah2211 <noah.decroix3@gmail.com>
2022-02-10 20:34:11 +0100

    done

--- index.php
+++ index.php
@@ -19,32 +19,57 @@ try {
     
     $sql1 = $insert . "('Decroix', 'Noah', 'noah.decroix3@gmail.com', 'dede', 'mon adresse', '59530', 'France')";
 
-    DB_Connect::dbConnect()->exec($sql1);
+    //DB_Connect::dbConnect()->exec($sql1);
 
     /**
      * 2. Insérez un nouveau produit dans la table produit
      */
-
-    // TODO votre code ici.
-
+    $insert2 = 'INSERT INTO produit (title, price, description_courte, description_longue) VALUES ';
+    $sql2 = $insert2 . "('titre', '12', 'descri courte', 'descri longue')";
+    //DB_Connect::dbConnect()->exec($sql2);
     /**
      * 3. En une seule requête, ajoutez deux nouveaux utilisateurs à la table utilisateur.
      */
 
-    // TODO Votre code ici.
+    $sql ="
+        INSERT INTO utilisateur (family_name, name, email, password, adresse, postal_code, country)
+         VALUES ('test', 'testtest', 'dede@gmail.com', 'dedede', 'monadresee', '55555', 'France'),
+                ('teste', 'testtestee', 'dedeeee@gmail.com', 'dededdesdse', 'monadresedddde', '555555', 'France')   
+    ";
 
+    //DB_Connect::dbConnect()->exec($sql);
     /**
      * 4. En une seule requête, ajoutez deux produits à la table produit.
      */
 
-    // TODO Votre code ici.
+    $request ="
+            INSERT INTO produit (title, price, description_courte, description_longue)
+             VALUES ('titre1', '34', 'descrcourte', 'descr longue'),
+                    ('titre2', '32', 'descrrr', 'descfzokfzeofz')
+    ";
+    DB_Connect::dbConnect()->exec($request);
 
     /**
      * 5. A l'aide des méthodes beginTransaction, commit et rollBack, insérez trois nouveaux utilisateurs dans la table utilisateur.
      */
 
-    // TODO Votre code ici.
+    DB_Connect::dbConnect()->beginTransaction();
 
+    $request2 = $insert . "
+    ('test', 'testtest', 'deddddd121212e@gmail.com', 'dedede', 'monadresee', '55555', 'France'),
+    ('test', 'testtest', 'ded22332122121e@gmail.com', 'dedede', 'monadresee', '55555', 'France'),
+    ('test', 'testtest', 'dede43412121234@gmail.com', 'dedede', 'monadresee', '55555', 'France')
+    ";
+
+    $request3 = $insert2 . "
+    ('produit1', '499', 'descri courte', 'descri longue),
+    ('produit2', '499', 'descri courte', 'descri longue),
+    ('produit3', '499', 'descri courte', 'descri longue)
+    
+    ";
+
+    DB_Connect::dbConnect()->exec($request2);
+    DB_Connect::dbConnect()->commit();
 
     /**
      * 6. A l'aide des méthodes beginTransaction, commit et rollBack, insérez trois nouveaux produits dans la table produit.
@@ -52,5 +77,6 @@ try {
     echo "La ou les requêtes a/ont fonctionné(es)";
 }
 catch (PDOException $e) {
+    DB_Connect::dbConnect()->rollBack();
     echo "Erreur: " . $e->getMessage();
 }
